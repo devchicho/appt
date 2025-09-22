71f71c51575cc0cc45257e944af173856b96bcf6
chinmayhem <chaudharychinmay@gmail.com>
2025-09-21 22:25:17 -0400

    logs

--- dataService.ts
+++ dataService.ts
@@ -11,7 +11,7 @@ const REGEX =
 
 const normalizeDate = (dateStr: string) => {
   const [y, m, d] = dateStr.split("-").map(Number);
-  return new Date(y, m - 1, d).toISOString().split("T")[0];
+  return new Date(y, m - 1, d).getTime();
 };
 
 const areDatesEqual = (date1: string, date2: string) => {
@@ -26,7 +26,7 @@ function processResponse(html: string, dateStr: string) {
   if (!areDatesEqual(receivedDate, dateStr)) {
     console.log("SLOTS NOT FOUND!!!");
     console.log(
-      "Received response for different date:",
+      `Received response for different date: ${receivedDate} - `,
       formatPrettyDate(receivedDate)
     );
     return [];
@@ -64,8 +64,7 @@ export async function loadData(dateStr: string): Promise<any> {
           "sec-fetch-site": "same-origin",
           "sec-fetch-user": "?1",
           "upgrade-insecure-requests": "1",
-          cookie:
-            "_ga=GA1.2.1855009062.1758494052; _gid=GA1.2.189296927.1758494052; .AspNetCore.Antiforgery.cdV5uW_Ejgc=CfDJ8LshbrkRxwRIt5-gCiYOVB4p-YillcQecl46TTSeZB4s-iSsZxC8spl9EDRfYADBDT6Q4wRHu786c-Hgp9YdNHetOzV6LjnhCCs4ptIPltirwfpjUOPLjHfNFTAHtxaM_Y8qZDQGL73CwSozFpTFjD4; .AspNetCore.Session=CfDJ8LshbrkRxwRIt5%2BgCiYOVB5pvjRQNj0okptb3JJQSw11nnYSA7E3QccwsT8F6zLENdoDTiOqZGZ%2Fx9SOHRYTdI1IUbet6Ql2ghbdirF6JeA4q8bkzI%2FR5W9BH0cknmc5pw9pPFA65rI95iCuECUGXdj5xI8u27tbEjFlyDiO1l44; ARRAffinity=cb579bba8b62ec477c0613fc2a3d4f30d1b6e2f1c7cadcbb10c6753161c5f16e; ARRAffinitySameSite=cb579bba8b62ec477c0613fc2a3d4f30d1b6e2f1c7cadcbb10c6753161c5f16e; ASLBSA=00037e785ebd1fbc0d6ff505750ab43a6ef1429d36cc2c6a1e130198ab6014ebdd0d; ASLBSACORS=00037e785ebd1fbc0d6ff505750ab43a6ef1429d36cc2c6a1e130198ab6014ebdd0d",
+          cookie: "",
           Referer:
             "https://telegov.njportal.com/njmvc/AppointmentWizard/12/125",
         },
